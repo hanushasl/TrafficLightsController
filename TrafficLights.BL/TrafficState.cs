a4485cbc60630d60af49e29ee8daba9482ff4ebc
hanushasl <hanus.hasl@gmail.com>
2021-12-06 22:25:31 +0100

    Add traffic state and transition command enums.

---
+++ TrafficLights.BL/TrafficState.cs
@@ -0,0 +1,13 @@
+﻿namespace TrafficLights.BL
+{
+  /// <summary>
+  /// Traffic states enum.
+  /// </summary>
+  public enum TrafficState
+  {
+    S0,
+    S1,
+    S2,
+    S3
+  }
+}
---
+++ TrafficLights.BL/TransitionCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace TrafficLights.BL
+{
+  /// <summary>
+  /// Transition commands enum.
+  /// </summary>
+  public enum TransitionCommand
+  {
+    T0,
+    T1,
+    T2,
+    T3
+  }
+}
