3acaa1f6a5af0f83cabd8177a2f89d3b47945a4f
hanushasl <hanus.hasl@gmail.com>
2021-12-06 23:47:46 +0100

    Add console client project.

---
+++ TrafficLights.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrafficLights.Console
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
---
+++ TrafficLights.ConsoleClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TrafficLights.BL;
+
+namespace TrafficLights.ConsoleClient
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome in traffic lights control system!!!");
+      Console.WriteLine("===========================================");
+
+      ITrafficIntersectionSystem tis = new TrafficIntersectionSystem();
+    }
+  }
+}
